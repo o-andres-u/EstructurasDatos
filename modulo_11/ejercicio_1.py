1c9db6fff34443d00fe1dccf9770a14fbbe6fd26
Oscar Usuga <oscar.usuga@nordstrom.com>
2022-06-23 19:51:27 -0500

    Adding new exercises

--- modulo_11/ejercicio_1.py
+++ modulo_11/ejercicio_1.py
@@ -1,5 +1,40 @@
-for i in range(int(input())):
-    I, B = map(int, input().split(', '))
-    for j in range(B):
-        couple = map(int, input().split())
+from collections import deque
 
+
+class Node:
+    def __init__(self, num):
+        self.num = num
+        self.visited = False
+        self.distance = 0
+        self.neighbors = []
+
+    def __str__(self):
+        return f'{self.num}:{self.distance}'
+
+
+def print_interaction(graph):  # iterate using BFS
+    q = deque()
+    start = 0
+    q.append(start)
+    while len(q) > 0:
+        current = q.popleft()
+        for node in graph[current]:
+            if not node.visited:
+                node.visited = True
+                q.append(node.num)
+
+
+def run():
+    for i in range(int(input())):
+        people, couples = map(int, input().split(', '))
+        graph = {}
+        for j in range(people):
+            graph[j] = Node(j)
+        for j in range(couples):
+            doc1, doc2 = map(int, input().split())
+            graph[doc1].neighbors.append(doc2)
+        print_interaction(graph)
+
+
+if __name__ == '__main__':
+    run()
